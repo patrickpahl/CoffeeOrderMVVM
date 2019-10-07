d943c092693764ccd8d4a17147c81eb722b5b874
Patrick Pahl <>
2019-10-06 21:31:05 -0500

    setup

---
+++ CoffeeOrderMVVM/ViewControllers/AddOrderViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddOrderViewController.swift
+//  CoffeeOrderMVVM
+//
+//  Created by Patrick Pahl on 10/6/19.
+//  Copyright © 2019 PYP. All rights reserved.
+//
+
+import UIKit
+
+class AddOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CoffeeOrderMVVM/ViewControllers/OrdersTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  OrdersTableViewController.swift
+//  CoffeeOrderMVVM
+//
+//  Created by Patrick Pahl on 10/6/19.
+//  Copyright © 2019 PYP. All rights reserved.
+//
+
+import UIKit
+
+class OrdersTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+
+}
